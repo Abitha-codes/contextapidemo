d9a31248963ace4c596d24fdf6ce28d4711e66d9
Abitha <pro.abitha@gmail.com>
2026-01-08 13:16:29 +0530

    Login Credential implement

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext} from 'react'
 import { LoginContext } from '../contexts/LoginContext'
 // import { useState } from 'react'
 import "../App.css"
 
 function Login() {
-    const {setName,setProfileInfo}=useContext(LoginContext)
+    const {name,setName,pwd,setPwd,profileinfo,setProfileInfo}=useContext(LoginContext)
     // const [name,setName]=useState("")
     // const [pwd,setPwd]=useState("")
     // const [profileinfo,setProfileinfo]=useState(false)
-    // const show=()=>{
-    //     if (name==="Abitha" && pwd==="Abi")
-    //         setProfileinfo(true)
-    //     else
-    //         setProfileinfo(false)
-    // }
+    //const [profileinfo,setProfileInfo]=useState(false)
+    //const [error,setError]=useState("")
+    const show=()=>{
+      if (name==="Abitha" && pwd==="Abi")
+          setProfileInfo(true)
+      else
+          setProfileInfo(false)
+        
+    }
   return (
     <div id='div'>
     <center>
       <h1>Login Form</h1>
       <label>User Name : </label>
-      <input type='text' 
-      onChange={(e)=>setName(e.target.value)} placeholder='User Name'></input><br/><br/>
+      <input type='text' onChange={(e)=>setName(e.target.value)} placeholder='User Name'></input><br/><br/>
       <label>Password : </label>
-      <input type='password' 
-    //   onChange={(e)=>setPwd(e.target.value)} value={pwd} 
-      placeholder='Password'></input><br/><br/>
-      <input type='button'onClick={(e)=>setProfileInfo(true)} value="Login"></input>
+      <input type='password' onChange={(e)=>setPwd(e.target.value)} value={pwd} placeholder='Password'></input><br/><br/>
+      <input type='button'onClick={()=>show()} value="Login"></input>
       <br/><br/>
+      {/* {error && <p style={{color:"red"}}>{error}</p>} */}
+      {profileinfo===false && <font color="red">Invalid...!!</font> }
       {/* {(profileinfo && "Valid")|| "Invalid"} */}
     </center>
     </div>
